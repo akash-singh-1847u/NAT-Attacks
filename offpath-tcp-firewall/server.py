635fd9f020870d1fb1f4959d7139636394105ecd
ManjunathByadagi <manjunathbyadagi11@gmail.com>
2026-07-13 09:52:13 +0530

    Refactor server handling and integration

--- offpath-tcp-firewall/server.py
+++ offpath-tcp-firewall/server.py
@@ -1,33 +1,31 @@
 import socket
 
-HOST = "0.0.0.0"
+HOST = "20.0.0.20"
 PORT = 8080
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 server.bind((HOST, PORT))
 server.listen(5)
 
-print("=" * 50)
-print("Server Listening on Port 8080")
-print("=" * 50)
+print("Listening...")
 
 while True:
-    conn, addr = server.accept()
 
-    print("\n" + "=" * 50)
-    print("Client Connected!")
-    print(f"Client IP   : {addr[0]}")
-    print(f"Client Port : {addr[1]}")
+    client, addr = server.accept()
+
+    print("=" * 40)
+    print("Client:", addr)
 
     while True:
-        data = conn.recv(1024)
+
+        data = client.recv(1024)
 
         if not data:
-            print("Client Disconnected")
             break
 
-        print(f"Received Data : {data.decode()}")
+        print("Received:", data.decode())
 
-        conn.send(b"Hello Client")
+        client.send(b"Hello client")
 
-    conn.close()
+    client.close()
